4a08424ed8406d16b2374fc92e9f1ceb7b136ddd
vicentefiorito <vicentefiorito@gmail.com>
2024-02-10 09:04:19 -0500

    implemented test for the cell grid and functionalities

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from graphics import Window,Line,Point
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    line1 = Line(Point(50,50),Point(400,400))
-    win.draw_line(line1,"black")
+
+    # testing cell
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50,50,100,100)
+
+    c1 = Cell(win)
+    c1.has_top_wall = False
+    c1.has_right_wall = False
+    c1.draw(150,150,200,200)
+
+    c2 = Cell(win)
+    c2.has_right_wall = False
+    c2.draw(250,250,300,300)
+
     win.wait_for_close()
 
 
