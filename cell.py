7ed22c63ca8443b2198cf275fe83b2aa2cbe1dad
vicentefiorito <vicentefiorito@gmail.com>
2024-02-10 10:07:03 -0500

    added implementation to move between cells depeding on the direction

--- cell.py
+++ cell.py
@@ -12,6 +12,7 @@ class Cell:
         self._y2 = None
         self._win = win
     
+    # draws a line onto the canvas
     def draw(self,x1,y1,x2,y2):
         self._x1 = x1
         self._y1 = y1
@@ -37,3 +38,55 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1,y2), Point(x2,y2))
             self._win.draw_line(line)
+    
+    # draws a path between two cells
+    def draw_move(self,to_cell,undo=False):
+        if self._win is None:
+            return 
+
+        # calculating midpoints
+        mid_x = (self._x1 + self._x2) / 2
+        mid_y = (self._y1 + self._y2) / 2
+        
+        to_x = (to_cell._x1 + to_cell._x2) / 2
+        to_y = (to_cell._y1 + to_cell._y2) / 2
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+        
+        # moving left 
+        if self._x1 > to_cell._x1:
+            # draws the line from the left side to the center
+            line = Line(Point(self._x1,mid_y),Point(mid_x,mid_y))
+            self._win.draw_line(line,fill_color)
+            # draws the line to the other cell
+            line = Line(Point(to_x,to_y), Point(to_cell._x2,to_y))
+            self._win.draw_line(line)
+        
+        # moving right
+        elif self._x1 < to_cell._x1:
+            # draws the line from the center of the current cell to the right
+            line = Line(Point(mid_x,mid_y), Point(self._x2,mid_y))
+            self._win.draw_line(line,fill_color)
+            # draw to the line to the other cell
+            line = Line(Point(to_cell._x1,to_y), Point(to_x,to_y))
+            self._win.draw_line(line,fill_color)
+        
+        # moving up
+        elif self._y1 > to_cell._y1:
+            # draws the line from the current cell to the midpoint
+            line = Line(Point(mid_x,mid_y), Point(mid_x,self._y1))
+            self._win.draw_line(line,fill_color)
+            # draws the line from the next cell to the midpoint
+            line = Line(Point(to_x,to_cell._y2), Point(to_x,to_y))
+            self._win.draw_line(line,fill_color)
+        
+        # moving down
+        elif self._y1 < to_cell._y1:
+            # draws the line from the current cell to the midpoint
+            line = Line(Point(mid_x,mid_y), Point(mid_x,self._y2))
+            self._win.draw_line(line,fill_color)
+            # draws the line from the next cell to the midpoint
+            line = Line(Point(to_x,to_y), Point(to_x,to_cell._y1))
+            self._win.draw_line(line,fill_color)
--- main.py
+++ main.py
@@ -5,21 +5,30 @@ from cell import Cell
 def main():
     win = Window(800, 600)
 
-    # testing cell
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50,50,100,100)
-
     c1 = Cell(win)
-    c1.has_top_wall = False
     c1.has_right_wall = False
-    c1.draw(150,150,200,200)
+    c1.draw(50, 50, 100, 100)
 
     c2 = Cell(win)
-    c2.has_right_wall = False
-    c2.draw(250,250,300,300)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
 
     win.wait_for_close()
-
-
+    
 main()
