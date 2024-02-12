69c4ac350e5b3da9a0c9c8ef61d4680aeb8e3564
vicentefiorito <vicentefiorito@gmail.com>
2024-02-12 09:45:00 -0500

    added some functionality tests to the maze class

---
+++ tests.py
@@ -0,0 +1,33 @@
+import unittest
+from maze import Maze
+
+# creating a test
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+    
+    def test_maze_create_large_cells(self):
+        num_cols = 20
+        num_rows = 16
+        m1 = Maze(0,0,num_rows,num_cols,20,20)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows
+        )
+    
+if __name__ == "__main__":
+    unittest.main()
