a96e08f6b00ee0edbf45f7ba47173c5c9febc424
vicentefiorito <vicentefiorito@gmail.com>
2024-02-12 09:29:25 -0500

    switched over from just having cells to implementing the whole maze

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
-from graphics import Window,Line,Point
-from cell import Cell
+from graphics import Window
+from maze import Maze
 
 
 def main():
-    win = Window(800, 600)
+    # implementing the Maze
+    num_cols = 12
+    num_rows = 4
+    margin = 50
+    cell_x = 100
+    cell_y = 300
+    screen_x = 1500
+    screen_y= 800
 
-    c1 = Cell(win)
-    c1.has_right_wall = False
-    c1.draw(50, 50, 100, 100)
-
-    c2 = Cell(win)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(100, 50, 150, 100)
-
-    c1.draw_move(c2)
-
-    c3 = Cell(win)
-    c3.has_top_wall = False
-    c3.has_right_wall = False
-    c3.draw(100, 100, 150, 150)
-
-    c2.draw_move(c3)
-
-    c4 = Cell(win)
-    c4.has_left_wall = False
-    c4.draw(150, 100, 200, 150)
-
-    c3.draw_move(c4, True)
-
+    win = Window(screen_x, screen_y)
+    maze = Maze(margin,margin,num_rows,num_cols,cell_x,cell_y,win)
     win.wait_for_close()
     
 main()
