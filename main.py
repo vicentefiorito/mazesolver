3391e4da8060ae1b01012bb7ad2fd351cae16cf5
vicentefiorito <vicentefiorito@gmail.com>
2024-02-12 09:49:26 -0500

    fixed bugs

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def main():
     num_cols = 12
     num_rows = 16
     margin = 50
-    cell_x = (screen_x - 2 * margin) / num_cols
-    cell_y = (screen_y - 2 * margin) / num_rows
     screen_x = 1500
     screen_y= 800
+    cell_x = (screen_x - 2 * margin) / num_cols
+    cell_y = (screen_y - 2 * margin) / num_rows
 
     win = Window(screen_x, screen_y)
     maze = Maze(margin,margin,num_rows,num_cols,cell_x,cell_y,win)
