bf86efc22538759f5d25ea134f534f79608d8f3e
vicentefiorito <vicentefiorito@gmail.com>
2024-02-12 08:41:47 -0500

    started initial maze implementation

---
+++ maze.py
@@ -0,0 +1,34 @@
+from cell import Cell
+import time
+
+class Maze:
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win,
+            ):
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+
+        self._create_cells()
+    
+    def _create_cells(self):
+        pass
+
+    def _draw_cell(self,i,j):
+        pass
+
+    def _animate(self):
+        self._win.redraw()
+        time.sleep(0.05)
