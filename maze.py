ee4328a7a5f982db47df1569e2246a8dc5b965cc
vicentefiorito <vicentefiorito@gmail.com>
2024-02-12 09:19:08 -0500

    implemented the maze by drawing the cells within the matrix

--- maze.py
+++ maze.py
@@ -31,9 +31,27 @@ class Maze:
                 col_cells.append(new_cell)
             self._cells.append(col_cells)
 
+        # drawing the cell in the matrix
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i,j)
 
     def _draw_cell(self,i,j):
-        pass
+        # if there is no window
+        if self._win is None:
+            return
+        # calculating the bottom x coordinate
+        x1 = self._x1 + i * self._cell_size_x
+        # calculating the bottom y coordinate
+        y1 = self._y1 + j * self._cell_size_y
+        # calculating the top x coordinate
+        x2 = x1 + self._cell_size_x
+        # calculating the top y coordinate
+        y2 = y1 + self._cell_size_y
+
+        # drawing the cell
+        self._cells[i][j].draw(x1,y1,x2,y2)
+        self._animate()
 
     def _animate(self):
         self._win.redraw()
